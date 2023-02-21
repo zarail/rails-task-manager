ccd670ad9672283a56fd0e2c3acfe781e3226f50
Zahra Ilkhan <zahraa1991@gmail.com>
2023-02-21 16:32:29 +0100

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: :new_task # displays the Task form
+  get '/tasks/:id', to: 'tasks#show', as: :task
+  post 'tasks', to: 'tasks#create' # handles the POST request generated when submitting the form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
